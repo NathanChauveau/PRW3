1276b05a3b7ea83563fcc1a68fd4f8d829da19a4
Nathan <nathan.chauveau@eduvaud.ch>
2026-03-03 22:55:29 +0100

    feat: add 3.12 (connect to mongoDB)

--- part3/phonebook-BE/mongo.js
+++ part3/phonebook-BE/mongo.js
@@ -1,31 +1,34 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-  console.log('give password as argument')
+if (process.argv.length < 5) {
+  console.log('give the necessary arguments to create a new record of a person')
   process.exit(1)
 }
 
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
 const url = `mongodb+srv://nathanchauveau_db_user:${password}@cluster0.rcgxpqj.mongodb.net/?appName=Cluster0`
 
-mongoose.set('strictQuery',false)
-
-mongoose.connect(url, { family: 4 })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+mongoose.set('strictQuery', false)
+mongoose.connect(url, {
+  serverSelectionTimeoutMS: 1000000000,
 })
 
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-note.save().then(result => {
-  console.log('note saved!')
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: name,
+  number: number,
+})
+
+person.save().then(() => {
+  console.log(`added ${name} number ${number} to phonebook`)
   mongoose.connection.close()
 })
